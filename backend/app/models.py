c49985a925bccdda2f8f294f792a5e7dffd56985
Danial Azmi <danial.azmiee@gmail.com>
2024-05-04 23:16:18 +0800

    add customer registration

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,22 @@
 from sqlmodel import Field, SQLModel
 from pydantic import BaseModel
+# from typing import Annotated
+from fastapi import Form
+
+from typing_extensions import Annotated, Doc
+
+from datetime import datetime, timezone
+
+def current_timestamp():
+    return datetime.now(timezone.utc).timestamp()
 
 ### Database tables
+
+class Login_credentials(SQLModel, table=True):
+    username: str = Field(primary_key=True)
+    password: str
+    login_attempt: int
+    last_login: int
 
 class Users(SQLModel, table=True):
     user_id: int | None = Field(default=None, primary_key=True)
@@ -12,6 +27,18 @@ class Users(SQLModel, table=True):
     age: int
     address: str
     email: str
+
+class Customers(SQLModel, table=True):
+    customer_id: str | None = Field(default=None, primary_key=True)
+    meter_serial: str | None
+    username: str = Field(foreign_key="login_credentials.username")
+    customer_email: str
+    first_name: str
+    last_name: str
+    address: str
+    ic_no: str
+    phone_no: str
+    age: int
 
 ### Generic schemas
 
@@ -29,6 +56,31 @@ class UserCreateRequest(SQLModel):
     age: int
     address: str
     email: str
+
+class RegisterCustomerForm:
+
+    def __init__(
+            self, 
+            username: Annotated[str, Form()],
+            password: Annotated[str, Form()],
+            customer_email: Annotated[str, Form()],
+            first_name: Annotated[str, Form()],
+            last_name: Annotated[str, Form()],
+            address: Annotated[str, Form()],
+            ic_no: Annotated[str, Form()],
+            phone_no: Annotated[str, Form()],
+            age: Annotated[int, Form()]
+            ):
+        self.username = username
+        self.password = password
+        self.customer_email = customer_email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.ic_no = ic_no
+        self.phone_no = phone_no
+        self.age = age
+
 
 class UserUpdateRequest(SQLModel):
     username: str | None = None
