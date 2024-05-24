a9ce0eef65b6a7e67cf3806e531fc32c20fb0a00
Danial Azmi <danial.azmiee@gmail.com>
2024-05-24 16:55:06 +0800

    add id generation util

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,5 +1,9 @@
 import yaml
+import random
 import hashlib
+from models import Customers
+# from core.main import engine
+from sqlmodel import Session, select
 
 CONFIG = 'config.yaml'
 
@@ -22,3 +26,32 @@ def hash_password(password: str):
     hashed = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
     return hashed
+
+def generate_id(engine, id_type: str):
+
+    mapping = {
+        'customer': {
+            'prefix': 'CUST',
+            'table': Customers,
+            'column': 'customer_id'
+        },
+        'employee': 'EMPL',
+        'meter': 'METR',
+        'tracking': 'TRKG'
+    }
+
+    _type = mapping[id_type]
+
+    statement = select(
+        getattr(_type['table'], _type['column'])
+    )
+
+    with Session(engine) as session:
+        results = session.exec(statement).all()
+
+    while True:
+
+        _id = _type['prefix'] + '%07d' % random.randint(0, 9999999)
+
+        if _id not in results:
+            return _id
